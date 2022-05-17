946c193fc86b037cd537ee220eda44d2f67bb6f7
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-05-16 19:32:03 -0500

    Add files via upload

---
+++ May16.js
@@ -0,0 +1,34 @@
+const question = new Map (
+    [ 'Question', 
+    'What is the best programming 
+    lanquage in the world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'Javascript'],
+    ['Correct' ,3]
+    [true, 'Correct'],
+    [false, 'try again'],
+);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries
+    (openingHours));
+const hourMap = new Map
+(Object.entries(openingHours));
+
+console.log(question.get(question.get('correct') === awnser));
+
+console.log(question.get('question'));
+
+
+for (const [key, values] of question) {
+    if (typeof key === 'number') 
+    console.log(`answer ${key}: 
+    ${value} `)
+}
+
+
+console.log(...question)
+console.log([...question.keys()]);
+console.log(...question.values)
